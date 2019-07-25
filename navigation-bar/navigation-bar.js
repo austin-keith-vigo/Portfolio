2f5d19b24534cb42518c05744f906fbf9a0c1de1
Austin Vigo <avigo@uci.edu>
2019-07-24 17:53:46 -0700

    animated the pressing of the nav-bar button
    animated the pressing of the nav-bar button

--- navigation-bar/navigation-bar.js
+++ navigation-bar/navigation-bar.js
@@ -1,12 +1,34 @@
 var isOpen = false;
 function toggleNavigationBarDisplay(){
     $navigationBar = $(".navigation-bar");
+    $navigationBarButtonOpen = $("#open");
+    $navigationBarButtonClose = $("#close");
+    
     if (isOpen == false){
         $navigationBar.css("display","block");
         isOpen = true;
+        
+        
+
+        $navigationBarButtonOpen.animate({
+            opacity: "0"
+        }, 200, function(){
+            $navigationBarButtonClose.css("opacity","1"); 
+        });
+
+        
     }
     else{
         $navigationBar.css("display","none");
         isOpen = false;
+        
+        
+        $navigationBarButtonClose.animate({
+            opacity: "0"
+        }, 200, function(){
+            $navigationBarButtonOpen.css("opacity","1")
+        });
     }
+    
+
 }
